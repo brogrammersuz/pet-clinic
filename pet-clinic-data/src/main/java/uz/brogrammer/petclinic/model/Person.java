b5df5ec37cc70a449722510c13a496fe72608370
brogrammers.uz <brogrammersuz@gmail.com>
2023-03-03 19:38:10 +0500

    Created pet clinic service interfaces. Closed #4

--- pet-clinic-data/src/main/java/uz/brogrammer/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/uz/brogrammer/petclinic/model/Person.java
@@ -2,8 +2,17 @@ package uz.brogrammer.petclinic.model;
 
 public class Person {
 
+    private Long id;
     private String firstName;
     private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getFirstName() {
         return firstName;
--- pet-clinic-data/src/main/java/uz/brogrammer/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/uz/brogrammer/petclinic/model/PetType.java
@@ -2,7 +2,16 @@ package uz.brogrammer.petclinic.model;
 
 public class PetType {
 
+    private Long id;
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
