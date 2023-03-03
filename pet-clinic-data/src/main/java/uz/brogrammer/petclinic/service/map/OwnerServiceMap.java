adb9cc826491a2b524cb6e1eaea4f537d9756f57
brogrammers.uz <brogrammersuz@gmail.com>
2023-03-03 19:54:04 +0500

    Implemented map based services

---
+++ pet-clinic-data/src/main/java/uz/brogrammer/petclinic/service/map/OwnerServiceMap.java
@@ -0,0 +1,39 @@
+package uz.brogrammer.petclinic.service.map;
+
+import uz.brogrammer.petclinic.model.Owner;
+import uz.brogrammer.petclinic.service.OwnerService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
+    }
+}
