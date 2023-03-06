bdf1248d92174ef64fdd64830170b9f94ad398f5
brogrammers.uz <brogrammersuz@gmail.com>
2023-03-06 18:39:17 +0500

    Implemented map based services. Closed #5

---
+++ pet-clinic-data/src/main/java/uz/brogrammer/petclinic/service/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package uz.brogrammer.petclinic.service.map;
+
+import uz.brogrammer.petclinic.model.Pet;
+import uz.brogrammer.petclinic.service.PetService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+}
