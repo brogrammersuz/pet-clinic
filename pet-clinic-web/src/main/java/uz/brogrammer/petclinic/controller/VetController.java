97fbb6771c4a61c7f5ccdcdb689dc18689208b65
brogrammers.uz <brogrammersuz@gmail.com>
2023-03-29 16:53:00 +0500

    Vets page is improved Closed #34

--- pet-clinic-web/src/main/java/uz/brogrammer/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/uz/brogrammer/petclinic/controller/VetController.java
@@ -2,8 +2,13 @@ package uz.brogrammer.petclinic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import uz.brogrammer.petclinic.model.Vet;
 import uz.brogrammer.petclinic.service.VetService;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("/vets")
@@ -17,10 +22,14 @@ public class VetController {
 
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String listVets(Model model) {
-        var list = vetService.findAll().stream()
-                .toList();
+        var list = vetService.findAll();
         model.addAttribute("vets", list);
         return "vet/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 
 }
